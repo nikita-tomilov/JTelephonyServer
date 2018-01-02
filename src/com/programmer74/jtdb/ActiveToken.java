f7d5fa0331f47ff2e8bf9b9d7b44b921647bea7b
Nikita Tomilov <programmer174@icloud.com>
2018-01-02 19:27:53 +0300

    db: get rid of Token usertype
    We don't need it. It was added because of reasons that we do not need to
    support anymore.

--- src/com/programmer74/jtdb/ActiveToken.java
+++ src/com/programmer74/jtdb/ActiveToken.java
@@ -19,18 +19,21 @@ public class ActiveToken implements java.io.Serializable {
     @GenericGenerator(name = "act_increment", strategy = "increment")
     private Integer id;
 
-    @Column(name="Token")
-    @org.hibernate.annotations.Type(type = "com.programmer74.jtdb.TokenUserType")
-    Token token;
+    @Column
+    private String TokenString;
 
     @Column
-    Integer CredentialID;
+    private Date ExpiresAt;
+
+    @Column
+    private Integer CredentialID;
 
     @Override
     public String toString() {
         return "ActiveToken{" +
                 "id=" + id +
-                ", token=" + token +
+                ", TokenString='" + TokenString + '\'' +
+                ", ExpiresAt=" + ExpiresAt +
                 ", CredentialID=" + CredentialID +
                 '}';
     }
@@ -43,12 +46,20 @@ public class ActiveToken implements java.io.Serializable {
         this.id = id;
     }
 
-    public Token getToken() {
-        return token;
+    public String getTokenString() {
+        return TokenString;
     }
 
-    public void setToken(Token token) {
-        this.token = token;
+    public void setTokenString(String tokenString) {
+        TokenString = tokenString;
+    }
+
+    public Date getExpiresAt() {
+        return ExpiresAt;
+    }
+
+    public void setExpiresAt(Date expiresAt) {
+        ExpiresAt = expiresAt;
     }
 
     public Integer getCredentialID() {
@@ -57,25 +68,5 @@ public class ActiveToken implements java.io.Serializable {
 
     public void setCredentialID(Integer credentialID) {
         CredentialID = credentialID;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        ActiveToken that = (ActiveToken) o;
-
-        if (id != null ? !id.equals(that.id) : that.id != null) return false;
-        if (token != null ? !token.equals(that.token) : that.token != null) return false;
-        return CredentialID != null ? CredentialID.equals(that.CredentialID) : that.CredentialID == null;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = id != null ? id.hashCode() : 0;
-        result = 31 * result + (token != null ? token.hashCode() : 0);
-        result = 31 * result + (CredentialID != null ? CredentialID.hashCode() : 0);
-        return result;
     }
 }
